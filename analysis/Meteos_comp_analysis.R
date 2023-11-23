37f37eae36f24167a6f75ae3b0959e8c4ad3ed47
lypluo <yluo@bgc-jena.mpg.de>
2023-11-23 16:37:45 +0100

    adding code

--- analysis/Meteos_comp_analysis.R
+++ analysis/Meteos_comp_analysis.R
@@ -21,11 +21,17 @@ names(df.Tha)
 #----------
 #(2)select the variables in two dataset and do the comparison:
 #----------
+##normalization the SWC:
+SWC.Dav.q95<-quantile(df.Dav$SWC_1,0.95,na.rm = T)
+SWC.Tha.q95<-quantile(df.Tha$SWC_F_MDS_1,0.95,na.rm = T)
+
 df.Dav_sel<-df.Dav %>%
   select(Date,SW_IN:VPD,
          #only select the first layer SWC and TS
          SWC_1,TS_1,D_SNOW
          )%>%
+  #normalization of SWC
+  mutate(SWC_Norm=SWC_1/SWC.Dav.q95)%>%
   mutate(sitename="DAV")
 
 df.Tha_sel<-df.Tha%>%
@@ -37,11 +43,12 @@ df.Tha_sel<-df.Tha%>%
         SWC_1=SWC_F_MDS_1,SWC_F_MDS_1=NULL,
         P_F=NULL
         )%>%
+  #normalization of SWC
+  mutate(SWC_Norm=SWC_1/SWC.Tha.q95)%>%
   mutate(sitename="THA")
 
 #
 df.all<-bind_rows(df.Tha_sel,df.Dav_sel)
-
 
 #----------
 #(3)plotting:
@@ -56,7 +63,7 @@ plot_fun<-function(df,var,x_f,legend_f){
   #
   df_sel<-df %>%
     filter(Date>=as.Date("2023-01-01")&Date<=as.Date("2023-09-01"))%>%
-    select(Date,starts_with(var),sitename)
+    select(Date,var,sitename)
   names(df_sel)<-c("Date","y","sitename")
   #plotting
   #
@@ -79,15 +86,28 @@ plot_fun<-function(df,var,x_f,legend_f){
     scale_color_manual(values = c("DAV"=adjustcolor("tomato",0.6),
                        "THA"=adjustcolor("cyan3",0.6)))+
     theme_light()+
-    theme(legend.position = c(0.8,0.2),legend.background = element_blank())
+    theme(legend.position = c(0.8,0.25),
+          legend.title = element_blank(),
+          legend.text = element_text(size = 18),
+          legend.background = element_blank(),
+          axis.text = element_text(size=16)
+          )
   ##########
   if(var=="TA"){
     df_plot<-df_plot+
-      ylab(expression("Ta ("* "°C)"))
+      ylab(expression("Ta ("* "°C)"))+
+      annotate(geom="text",x=df_sel_Tha_Samp$Date,y=df_sel_Tha_Samp$y+2,
+               label=paste0("C",1:6),col="cyan4",size=5)+
+      annotate(geom="text",x=df_sel_Dav_Samp$Date,y=df_sel_Dav_Samp$y+2,
+               label=paste0("C",1:6),col="tomato",size=5)
   }
-  if(var=="TS"){
+  if(var=="TS_1"){
     df_plot<-df_plot+
-      ylab(expression("Ts ("* "°C)"))
+      ylab(expression("Ts ("* "°C)"))+
+      annotate(geom="text",x=df_sel_Tha_Samp$Date,y=df_sel_Tha_Samp$y+2,
+               label=paste0("C",1:6),col="cyan4",size=5)+
+      annotate(geom="text",x=df_sel_Dav_Samp$Date,y=df_sel_Dav_Samp$y+2,
+               label=paste0("C",1:6),col="tomato",size=5)
   }
   if(var=="SW_IN"){
     df_plot<-df_plot+
@@ -99,15 +119,35 @@ plot_fun<-function(df,var,x_f,legend_f){
   }
   if(var=="PPFD_IN"|var=="PPFD_OUT"|var=="PPFD_DIF"){
     df_plot<-df_plot+
-      ylab(expression("PPFD ("*mu*"mol m"^-2*s^-1))
+      ylab(expression("PPFD ("*mu*"mol m"^-2*s^-1))+
+      annotate(geom="text",x=df_sel_Tha_Samp$Date,y=df_sel_Tha_Samp$y+50,
+               label=paste0("C",1:6),col="cyan4",size=5)+
+      annotate(geom="text",x=df_sel_Dav_Samp$Date,y=df_sel_Dav_Samp$y+50,
+               label=paste0("C",1:6),col="tomato",size=5)
   }
-  if(var=="SWC"){
+  if(var=="SWC_1"){
     df_plot<-df_plot+
-      ylab(expression("SWC"))
+      ylab(expression("SWC"))+
+      annotate(geom="text",x=df_sel_Tha_Samp$Date,y=df_sel_Tha_Samp$y+2,
+               label=paste0("C",1:6),col="cyan4",size=5)+
+      annotate(geom="text",x=df_sel_Dav_Samp$Date,y=df_sel_Dav_Samp$y+2,
+               label=paste0("C",1:6),col="tomato",size=5)
+  }
+  if(var=="SWC_Norm"){
+    df_plot<-df_plot+
+      ylab(expression("Norm SWC"))+
+      annotate(geom="text",x=df_sel_Tha_Samp$Date,y=df_sel_Tha_Samp$y+0.02,
+               label=paste0("C",1:6),col="cyan4",size=5)+
+      annotate(geom="text",x=df_sel_Dav_Samp$Date,y=df_sel_Dav_Samp$y+0.02,
+               label=paste0("C",1:6),col="tomato",size=5)
   }
   if(var=="D_SNOW"){
     df_plot<-df_plot+
-      ylab(expression("Snow depth (cm)"))
+      ylab(expression("Snow depth (cm)"))+
+      annotate(geom="text",x=df_sel_Tha_Samp$Date,y=df_sel_Tha_Samp$y+2,
+               label=paste0("C",1:6),col="cyan4",size=5)+
+      annotate(geom="text",x=df_sel_Dav_Samp$Date,y=df_sel_Dav_Samp$y+2,
+               label=paste0("C",1:6),col="tomato",size=5)
   }
   #########
   if(x_f==FALSE){
@@ -126,12 +166,14 @@ p_TA<-plot_fun(df.all,"TA",FALSE,TRUE)
 p_SW_IN<-plot_fun(df.all,"SW_IN",FALSE,FALSE)
 p_VPD<-plot_fun(df.all,"VPD",FALSE,FALSE)
 p_PAR<-plot_fun(df.all,"PPFD_IN",FALSE,FALSE)
-p_TS<-plot_fun(df.all,"TS",FALSE,FALSE)
-p_SWC<-plot_fun(df.all,"SWC",FALSE,FALSE)
+p_TS<-plot_fun(df.all,"TS_1",FALSE,FALSE)
+p_SWC<-plot_fun(df.all,"SWC_1",FALSE,FALSE)
+p_SWC_Norm<-plot_fun(df.all,"SWC_Norm",FALSE,FALSE)
 p_Snow<-plot_fun(df.all,"D_SNOW",TRUE,FALSE)
 #merge the plots:
 library(cowplot)
-plot_merge<-plot_grid(p_TA,p_PAR,p_TS,p_SWC,p_Snow,ncol=1,align = "hv")
+plot_merge<-plot_grid(p_TA,p_PAR,p_TS,p_SWC_Norm,p_Snow,ncol=1,align = "hv")
 #save the plot
 save.path<-"./manuscript/"
-ggsave(plot_merge,filename = paste0(save.path,"Meteo.png"),height = 9,width = 12)
+ggsave(plot_merge,filename = paste0(save.path,"Meteo.png"),height = 11,width = 12)
+
